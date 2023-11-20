fb20ff64e99ef5de93c4db51cc99b0c890ffde0d
xkrfer <xkrfer@gmail.com>
2023-11-20 15:33:04 +0800

    feat: set tz

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,19 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault("Asia/Shanghai");
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function formatDate(date: string | number | Date) {
+  // TZ为上海
   return dayjs(date).format("YYYY-MM-DD HH:mm:ss");
 }
